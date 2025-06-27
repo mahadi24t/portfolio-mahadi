bd729e8313a7988ee1c15adae044747befe13397
mahadi24t <mahadi24t@outlook.com>
2025-06-27 16:34:06 +0600

    dark theme default

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,7 +188,6 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
